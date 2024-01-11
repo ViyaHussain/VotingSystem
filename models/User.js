7a1a90ca6fce44709934e124a6647f3c0ff1cc7d
ViyaHussain <153489504+ViyaHussain@users.noreply.github.com>
2024-01-11 14:46:37 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://viyahussain:haviya@cluster0.rckrc1i.mongodb.net/votingsystem?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("connected to the db")
+})
+.catch((err)=>console.log(err))
+
+var Schema=mongoose.Schema;
+var userSchema=new Schema({
+    username:{type: String, required:true},
+    password:{type: String, required:true},
+    rollNo:{type: String, required:true},
+    Dept:{type: String, required:true},
+    email:{type: String, required:true},
+})
+
+let userModel=mongoose.model('tbl_User',userSchema);
+
+module.exports=userModel;
